13746977e6b49ac284de088c53b063e0c0e0856e
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-09-04 01:23:47 -0700

    assignment

---
+++ assignment 1/app.js
@@ -0,0 +1,17 @@
+// Question 1
+// alert("Hello! Welcome to my Website.")
+
+// Question 2
+// alert("Error! Please enter a valid password.");
+
+// Question 3
+// alert("Welcome to JS Land...\nHapy Coding!");
+
+// Question 4
+// alert("Welcome to JS Land...");
+
+// Question 5
+// alert("Hapy Coding!\nPrevent this page from creating additional dialogs.");
+
+// // Question 6
+// alert("Hello...I can run JS through my web brower's console")
---
+++ assignment 2/app.js
@@ -0,0 +1,39 @@
+// Question 1
+// var username;
+
+// Question 2
+// var myName = "Jhone Doe";
+
+// question 3
+// a
+// var message;
+// b
+// message = "Hello World";
+// c
+// alert(message);
+
+// Question 4
+// var studentName = "Jhone Doe";
+// var studentAge ="15 years old";
+// var studentCourse ="Certified Mobile Application Development";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+
+// Question 5
+// var food ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+
+// Question 6
+// var email ="example@example.com";
+// alert("My email address is " + email);
+
+// Question 7
+// var book ="A smater way to learn JavaScript";
+// alert("I am trying to learn from the Book"+ book);
+
+// Question 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Question 9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
