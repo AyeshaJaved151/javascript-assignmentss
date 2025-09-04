feb0582e7bca1eab2724dd6d2f5658276ba427e3
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-09-04 02:16:51 -0700

    assignment

---
+++ assignment 3/app.js
@@ -0,0 +1,22 @@
+// Question 1
+// let age ="15"
+// alert("I am " +age+ " years old")
+
+// Question 2
+// let visitPage ="14"
+// alert("You have visited this site "+visitPage+ " times")
+
+// Question 3
+// let birthYear = "1990"
+// document.write("My birth year is " + birthYear + "<br>")
+// document.write("Data type of my declared variable is number")
+
+// Question 4
+// let visitorName = "John Doe"
+// let productTitle = "T-shirt"
+// let quantity = "5"
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store")
+
+
+
+
