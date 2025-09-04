cd86295da88a575411e06e393e95e2ee372cd354
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-09-04 06:23:19 -0700

    assignment

---
+++ assignment 4/app.js
@@ -0,0 +1,31 @@
+// Question 1
+// let x=10 
+// let y=20
+// let z=30
+
+// Question 2
+// Legal variable names:
+// let myName;
+// let _age;
+// let $salary;
+// let user1;
+// let first_name;
+// Illegal variable names:
+// let 1name;
+// let my-name;
+// let var;
+// let full name;
+// let @price;
+
+// Question 3
+// a)
+// document.write(" Rules For naming JS variables   <br/>  <br/>  <br/>   ")
+// b)
+// document.write(" <br/> Variable names can only contain, numbers, $ and _. For example: $my_1stVariable")
+// c)
+// document.write(" <br/> Variables must begin with a letter, $ or _. For example: $name, _name or name")
+// e)
+// document.write(" <br/> Variable names are case sensitive")
+// d)
+// document.write(" <br/>Variable names should not be JS keywords")
+
