bc1652afd8c69c58ddafe75b3c2bd84f5a93b232
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-09-09 01:04:29 -0700

    assignment

---
+++ assignment 5/app.js
@@ -0,0 +1,123 @@
+ //  Question 1
+    // let num1 = 3;
+    // let num2 = 5;
+    // let sum = num1 + num2;
+    // document.write("Sum of " + num1 + " and " + num2 + " is " + sum + "<br><br>");
+
+ // Question 2 
+    // let subtraction = num1 - num2;
+    // let multiplication = num1 * num2;
+    // let division = num1 / num2;
+    // let modulus = num1 % num2;
+    // document.write("Subtraction of " + num1 + " and " + num2 + " is " + subtraction + "<br>");
+    // document.write("Multiplication of " + num1 + " and " + num2 + " is " + multiplication + "<br>");
+    // document.write("Division of " + num1 + " and " + num2 + " is " + division + "<br>");
+    // document.write("Modulus of " + num1 + " and " + num2 + " is " + modulus + "<br><br>");
+
+ // Question 3 
+    // let value; 
+    // document.write("Value after variable declaration is: " + value + "<br>");
+    // value = 5;
+    // document.write("Initial value: " + value + "<br>");
+    // value++; 
+    // document.write("Value after increment is: " + value + "<br>");
+    // value += 7; 
+    // document.write("Value after addition is: " + value + "<br>");
+    // value--; 
+    // document.write("Value after decrement is: " + value + "<br>");
+    // let remainder = value % 3;
+    // document.write("The remainder is: " + remainder + "<br>");   
+
+ // Question 4 
+    // let ticketPrice = 600;  
+    // let numberOfTickets = 5; 
+    // let totalCost = ticketPrice * numberOfTickets;
+    // document.write("Total cost to buy " + numberOfTickets + " tickets to a movie is " + totalCost + " PKR<br><br>");  
+
+ // Question 5 
+    // let tableNumber = 4; 
+    // document.write("Table of " + tableNumber + "<br>");
+    // for (let i = 1; i <= 10; i++) {
+    //  document.write(tableNumber + " x " + i + " = " + (tableNumber * i) + "<br>");  }   
+
+ // Question 6 
+    // (a) 
+    // let celsius = 25;
+    // (b) 
+    // let fahrenheit = (celsius * 9/5) + 32;
+    // document.write(celsius + "°C is " + fahrenheit + "°F<br>");
+    // (c) 
+    // let fahrenheitTemp = 70;
+    // (d) 
+    // let celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+    // document.write(fahrenheitTemp + "°F is " + celsiusTemp + "°C<br><br>");  
+
+ // Question 7 
+    // let priceItem1 = 650;
+    // let priceItem2 = 100;
+    // let quantityItem1 = 3;
+    // let quantityItem2 = 7;
+    // let shippingCharges = 100;
+    // let totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
+    // document.write("<h3>Shopping Cart Receipt</h3>");
+    // document.write("Price of item 1 is " + priceItem1 + "<br>");
+    // document.write("Quantity of item 1 is " + quantityItem1 + "<br>");
+    // document.write("Price of item 2 is " + priceItem2 + "<br>");
+    // document.write("Quantity of item 2 is " + quantityItem2 + "<br>");
+    // document.write("Shipping Charges " + shippingCharges + "<br><br>");
+    // document.write("Total cost of your order is " + totalCost + " PKR<br>");
+
+ //  Question 8
+    // let totalMarks = 980;
+    // let  obtainedMarks = 804;
+    // let  percentage = (obtainedMarks / totalMarks) * 100;
+    // document.write("Total Marks: " + totalMarks + "<br>");
+    // document.write("Marks Obtained: " + obtainedMarks + "<br>");
+    // document.write("Percentage: " + percentage + "%");    
+
+ // Question 9
+    // let usd = 10;
+    // let riyal = 25;
+    // let totalPKR = (usd * 104.80) + (riyal * 28);
+    // document.write("Total Currency in PKR: " + totalPKR);
+
+ // Question 10
+    // let num = 7;
+    // let result = ((num + 5) * 10) / 2;
+    // document.write("Original Number: " + num + "<br>");
+    // document.write("Final Result: " + result);
+
+ // Question 11
+    // let currentYear = 2016;
+    // let birthYear = 1992;
+    // let age1 = currentYear - birthYear;
+    // let age2 = age1 - 1; 
+    // document.write("Current Year: " + currentYear + "<br>");
+    // document.write("Birth Year: " + birthYear + "<br>");
+    // document.write("Your Age is: 24");
+
+ // Question 12
+    // let   radius = 20; 
+    // let pi = 3.142;
+    // let circumference = 2 * pi * radius;
+    // let area = pi * radius * radius;
+    // document.write("Radius of Circle: " + radius + "<br>");
+    // document.write("The Circumference is " + circumference + "<br>");
+    // document.write("The Area is " + area);
+
+ // Question 13
+    // a.
+    // var favoriteSnack = "chocolate chip";
+    // b.
+    // var currentAge = 15;
+    // c. 
+    // var maxAge = 65;
+    // d.
+    // var snacksPerDay = 3;
+    // e. 
+    // var totalSnacks = (maxAge - currentAge) * 365 * snacksPerDay;
+    // document.write("Favourite Snack: " + favoriteSnack + "<br>");
+    // document.write("Current Age: " + currentAge + "<br>");
+    // document.write("Estimated Maximum Age: " + maxAge + "<br>");
+    // document.write("Amount of snacks per day: " + snacksPerDay + "<br>");
+    // document.write("You will need " + totalSnacks + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge);
