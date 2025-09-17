88d457e75e23923101dd44d547058fe5e4bc7890
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-09-16 23:58:58 -0700

    assignment

---
+++ assignment 6/app.js
@@ -0,0 +1,56 @@
+// Question 1
+// let a = 10;
+//   document.write("Result:<br>");
+//   document.write("The value of a is: " + a + "<br>");
+//   document.write("...................................<br><br>");
+
+//   document.write("The value of ++a is: " + (++a) + "<br>");
+//   document.write("Now the value of a is: " + a + "<br><br>");
+
+//   document.write("The value of a++ is: " + (a++) + "<br>");
+//   document.write("Now the value of a is: " + a + "<br><br>");
+
+//   document.write("The value of --a is: " + (--a) + "<br>");
+//   document.write("Now the value of a is: " + a + "<br><br>");
+
+//   document.write("The value of a-- is: " + (a--) + "<br>");
+//   document.write("Now the value of a is: " + a + "<br>");
+
+// Question 2
+// let a = 2, b = 1;
+// let result = --a - --b + ++b + b--;
+//   document.write("a is " + a + "<br>");
+//   document.write("b is " + b + "<br>");
+//   document.write("result is " + result + "<br>");
+
+// Question 3
+// let userName = prompt("Enter your name:");
+// alert("Hello " + userName + "! Welcome to our website.");
+
+// Question 6
+// a) 
+// let sub1 = prompt("Enter first subject name:");
+// let sub2 = prompt("Enter second subject name:");
+// let sub3 = prompt("Enter third subject name:");
+
+// b) 
+// let totalMarks = 100;
+
+// c & d) 
+// let marks1 = Number(prompt("Enter obtained marks for " + sub1));
+// let marks2 = Number(prompt("Enter obtained marks for " + sub2));
+// let marks3 = Number(prompt("Enter obtained marks for " + sub3));
+
+// e)
+// let totalObtained = marks1 + marks2 + marks3;
+// let grandTotal = totalMarks * 3;
+// let percentage = (totalObtained / grandTotal) * 100;
+// document.write("<table border='1' cellspacing='0' cellpadding='5'>");
+// document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>");
+// document.write("<tr><td>" + sub1 + "</td><td>" + totalMarks + "</td><td>" + marks1 + "</td><td>" + ((marks1/totalMarks)*100).toFixed(2) + "%</td></tr>");
+// document.write("<tr><td>" + sub2 + "</td><td>" + totalMarks + "</td><td>" + marks2 + "</td><td>" + ((marks2/totalMarks)*100).toFixed(2) + "%</td></tr>");
+// document.write("<tr><td>" + sub3 + "</td><td>" + totalMarks + "</td><td>" + marks3 + "</td><td>" + ((marks3/totalMarks)*100).toFixed(2) + "%</td></tr>");
+
+// document.write("<tr><th>Total</th><th>" + grandTotal + "</th><th>" + totalObtained + "</th><th>" + percentage.toFixed(2) + "%</th></tr>");
+// document.write("</table>");
+
