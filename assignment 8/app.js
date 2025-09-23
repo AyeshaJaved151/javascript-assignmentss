e7f9d5113b0518f32a880021feebd75805c58465
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-09-23 06:49:04 -0700

    assignment

---
+++ assignment 8/app.js
@@ -0,0 +1,56 @@
+// Question 1
+// let char = prompt("Enter a character:");
+// let result = (char >= '0' && char <= '9') ? "It is a number."
+//            : (char >= 'A' && char <= 'Z') ? "It is an uppercase letter."
+//            : (char >= 'a' && char <= 'z') ? "It is a lowercase letter."
+//            : "Invalid input.";
+// console.log(result);
+
+// Question 2
+// let num1 = +prompt("Enter first integer:");
+// let num2 = +prompt("Enter second integer:");
+// let result = (num1 > num2) ? `${num1} is larger.`
+//            : (num2 > num1) ? `${num2} is larger.`
+//            : "Both are equal.";
+// console.log(result);
+
+// Qustion 3
+// let number = +prompt("Enter a number:");
+// let result = (number > 0) ? "The number is positive."
+//            : (number < 0) ? "The number is negative."
+//            : "The number is zero.";
+// console.log(result);
+
+// Question 4
+// let ch = prompt("Enter a character:");
+// let result = (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') 
+//              ? true 
+//              : false;
+// console.log(result);
+
+// Question 5
+// let correctPassword = "hello123";
+// let userPassword = prompt("Enter your password:");
+// let result = (!userPassword) ? "Please enter your password"
+//            : (userPassword === correctPassword) 
+//              ? "Correct! The password you entered matches the original password"
+//              : "Incorrect password";
+// console.log(result);
+
+// Question 6
+// let hour = 13;
+// let greeting = (hour < 18) ? "Good day" : "Good evening";
+// console.log(greeting);
+
+// Question 7
+// let time = +prompt("Enter time in 24-hour format (e.g., 1900):");
+// let greeting = (time >= 0 && time < 1200) ? "Good Morning"
+//              : (time >= 1200 && time < 1700) ? "Good Afternoon"
+//              : (time >= 1700 && time < 2100) ? "Good Evening"
+//              : (time >= 2100 && time <= 2359) ? "Good Night"
+//              : "Invalid time format";
+// console.log(greeting);
+
+
+
+
