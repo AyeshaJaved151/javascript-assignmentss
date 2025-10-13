b7cea986e77b2f38367e32ecb7590e45a906e94c
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-10-13 11:09:51 -0700

    assignment

---
+++ assignment 12/app.js
@@ -0,0 +1,54 @@
+// Question 1
+// let num = +prompt("Enter a positive number:");
+// let roundOff = Math.round(num);
+// let floorValue = Math.floor(num);
+// let ceilValue = Math.ceil(num);
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + roundOff + "<br>");
+// document.write("Floor value: " + floorValue + "<br>");
+// document.write("Ceil value: " + ceilValue);
+
+// Question 2
+// let num = +prompt("Enter a negative floating point number:");
+// let roundOff = Math.round(num);
+// let floorValue = Math.floor(num);
+// let ceilValue = Math.ceil(num);
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + roundOff + "<br>");
+// document.write("Floor value: " + floorValue + "<br>");
+// document.write("Ceil value: " + ceilValue);
+
+// Question 3
+// let num = +prompt("Enter a number to find its absolute value:");
+// let absoluteValue = Math.abs(num);
+// document.write("The absolute value of " + num + " is " + absoluteValue);
+
+// Question 4
+// let diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("Random dice value: " + diceValue);
+
+// Question 5
+// let toss = Math.floor(Math.random() * 2) + 1;
+// if (toss === 1) {
+//   document.write(toss + "<br>Random coin value: Tails");
+// } else {
+//   document.write(toss + "<br>Random coin value: Heads");
+// }
+
+// Question 6
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+// document.write("Random number between 1 and 100: " + randomNumber);
+
+// Question 7
+// let userInput = prompt("Enter your weight in kilograms:");
+// let weight = parseFloat(userInput);
+// document.write("The weight of user is " + weight + " kilograms")
+
+// Question 8
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+// let userGuess = parseInt(prompt("Enter a number between 1 and 10:"));
+// if (userGuess === secretNumber) {
+//   alert("Congratulations! You guessed the secret number!");
+// } else {
+//   alert("Sorry, the secret number was " + secretNumber);
+// }
