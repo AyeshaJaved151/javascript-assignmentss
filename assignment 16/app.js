c7a17d6c86216efe426978828a8a634ec0b9d5f5
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-11-09 13:47:13 -0800

    assignment

---
+++ assignment 16/app.js
@@ -0,0 +1,27 @@
+// Question 1
+// let alertLink = document.getElementById("alertLink");
+// alertLink.addEventListener("click", function() {
+//   alert("You clicked on the link!");
+// });
+
+// Question 2
+// let mobileImages = document.querySelectorAll("#mobiles img");
+// mobileImages.forEach(function(img) {
+//   img.addEventListener("click", function() {
+//     alert("Thanks for browsing " + img.alt + " image!");
+//   });
+// });
+
+
+// Qiestion 4
+// let hoverImage = document.getElementById("hoverImage");
+
+// hoverImage.addEventListener("mouseover", function() {
+//   hoverImage.src = "http://127.0.0.1:5500/assignment%2016/script.js";
+// });
+
+// hoverImage.addEventListener("mouseout", function() {
+//   hoverImage.src = "http://127.0.0.1:5500/assignment%2016/script.js";
+// });
+
+
