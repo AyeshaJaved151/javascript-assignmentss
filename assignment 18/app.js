9d05f9572c4468d8549fedbf91461bd42e3a70ea
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-11-09 14:12:23 -0800

    assignment

---
+++ assignment 18/app.js
@@ -0,0 +1,56 @@
+// // Question 1
+// // 1
+// let mainContent = document.getElementById("main-content");
+// console.log(mainContent);
+
+// // 2
+// let children = mainContent.children;
+// console.log("Children of main-content:");
+// for (let i = 0; i < children.length; i++) {
+//     console.log(children[i]);
+// }
+
+// // 3
+// let renderElements = document.getElementsByClassName("render");
+// for (let i = 0; i < renderElements.length; i++) {
+//     console.log(renderElements[i].innerHTML);
+// }
+
+// // 4
+// let firstNameInput = document.getElementById("first-name");
+// firstNameInput.value = "Alex";
+
+// // 5
+// let lastNameInput = document.getElementById("last-name");
+// lastNameInput.value = "Bank";
+
+// let emailInput = document.getElementById("email");
+// emailInput.value = "alexbank@example.com";
+
+
+
+
+// // Question 2
+// // 1
+// let formContent = document.getElementById("form-content");
+// console.log("Node type of form-content:", formContent.nodeType);
+
+// // 2
+// let lastNameElement = document.getElementById("lastName");
+// console.log("Node type of lastName:", lastNameElement.nodeType);
+// console.log("Node type of lastName child:", lastNameElement.firstChild.nodeType);
+
+// // 3
+// lastNameElement.textContent = "Last Name: Javed";
+
+// // 4
+// console.log("First child:", mainContent.firstElementChild);
+// console.log("Last child:", mainContent.lastElementChild);
+
+// // 5
+// console.log("Next sibling:", lastNameElement.nextElementSibling);
+// console.log("Previous sibling:", lastNameElement.previousElementSibling);
+
+// // 6
+// console.log("Parent node of email:", emailInput.parentNode);
+// console.log("Node type of email:", emailInput.nodeType);
