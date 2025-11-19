1f1614051f88c785f12991352915688c788dfd6d
AyeshaJaved151 <aishajawaid483@gmail.com>
2025-11-19 14:02:38 +0500

    assignment

---
+++ assignment 19/app.js
@@ -0,0 +1,59 @@
+// Question 1
+// let itemsArray = [
+//   { name: "juice", price: 50, quantity: 3 },
+//   { name: "cookie", price: 30, quantity: 9 },
+//   { name: "shirt", price: 880, quantity: 1 },
+//   { name: "pen", price: 100, quantity: 2 }
+// ];
+// let totalAllItems = 0;
+// for (let i = 0; i < itemsArray.length; i++) {
+//   let itemTotal = itemsArray[i].price * itemsArray[i].quantity;
+//   console.log(itemsArray[i].name + " total = " + itemTotal);
+
+//   totalAllItems = totalAllItems + itemTotal;
+// }
+// console.log("Total price of all items = " + totalAllItems);
+
+
+// Question 2
+// let user = {
+//   name: "Aisha",
+//   email: "aisha@gmail.com",
+//   password: "12345",
+//   age: 15,
+//   gender: "female",
+//   city: "Karachi",
+//   country: "Pakistan"
+// };
+// console.log("age" in user);       
+// console.log("country" in user);   
+// console.log("firstName" in user); 
+// console.log("lastName" in user);  
+
+
+// Question 3
+// function Student(name, age, city) {
+//   this.name = name;
+//   this.age = age;
+//   this.city = city;
+// }
+// let std1 = new Student("Ali", 18, "Lahore");
+// let std2 = new Student("Sara", 20, "Karachi");
+// let std3 = new Student("Bilal", 22, "Islamabad");
+// console.log(std1);
+// console.log(std2);
+// console.log(std3);
+
+
+// Question 4
+// function  Person(name, gender, address, education, profession) {
+//   this.name = name;
+//   this.gender = gender;
+//   this.address = address;
+//   this.education = education;
+//   this.profession = profession;
+// }
+// let p1 = new Person("Ali", "Male", "Lahore", "Matric", "student");
+// let p2 = new Person("Aisha", "Female", "Karachi", "Intermediate", "web developer");
+// console.log(p1);
+// console.log(p2);
